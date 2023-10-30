35ea9fe9fc6f0e8c3bbb275dbd66caa2a9fcbb63
MFMcNolt <141301431+MFMcNolt@users.noreply.github.com>
2023-10-30 18:49:57 +1100

    added in a shapes.js file

--- index.js
+++ index.js
@@ -1,76 +1,69 @@
 const fs = require('fs');
 const { prompt } = require('inquirer');
+const { Circle, Triangle, Square } = require('./shapes');
 
 async function generateLogo() {
-  // Prompt the user for input
-  const userInput = await prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters for the text:',
-      validate: (input) => input.length <= 3, // Validate input length
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (keyword or hexadecimal):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Select a shape:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (keyword or hexadecimal):',
-    },
-  ]);
+    // Prompt the user for input
+    const userInput = await prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the text:',
+            validate: (input) => input.length <= 3, // Validate input length
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color (keyword or hexadecimal):',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select a shape:',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter shape color (keyword or hexadecimal):',
+        },
+    ]);
 
-  // Generate SVG markup based on user input
-  let svgString;
+    // Generate SVG markup based on user input
+    let svgString;
 
-  switch (userInput.shape) {
-    case 'circle':
-      svgString = `<?xml version="1.0" encoding="UTF-8"?>
-        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-          <circle cx="150" cy="100" r="50" fill="${userInput.shapeColor}" />
-          <text x="150" y="100" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
-        </svg>`;
-      break;
+    switch (userInput.shape) {
+        case 'circle':
+            const circle = new Circle(userInput);
+            svgString = circle.generateSvgString();
+            break;
 
-    case 'triangle':
-      svgString = `<?xml version="1.0" encoding="UTF-8"?>
-        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-          <polygon points="150,50 100,150 200,150" fill="${userInput.shapeColor}" />
-          <text x="150" y="100" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
-        </svg>`;
-      break;
+        case 'triangle':
+            const triangle = new Triangle(userInput);
+            svgString = triangle.generateSvgString();
+            break;
 
-    case 'square':
-      svgString = `<?xml version="1.0" encoding="UTF-8"?>
-        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-          <rect x="100" y="50" width="100" height="100" fill="${userInput.shapeColor}" />
-          <text x="150" y="100" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
-        </svg>`;
-      break;
+        case 'square':
+            const square = new Square(userInput);
+            svgString = square.generateSvgString();
+            break;
 
-    default:
-      console.error('Invalid shape');
-      return;
-  }
-
-  // Save the SVG to a file
-  const fileName = 'logo.svg';
-
-  fs.writeFile(fileName, svgString, (err) => {
-    if (err) {
-      console.error('Error saving SVG:', err);
-    } else {
-      console.log('Generated', fileName);
+        default:
+            console.error('Invalid shape');
+            return;
     }
-  });
+
+    // Save the SVG to a file
+    const fileName = 'logo.svg';
+
+    fs.writeFile(fileName, svgString, (err) => {
+        if (err) {
+            console.error('Error saving SVG:', err);
+        } else {
+            console.log('Generated', fileName);
+        }
+    });
 }
+
 // Run the logo generation function
 generateLogo();
